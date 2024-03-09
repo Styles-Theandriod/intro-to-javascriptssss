2632e08582e1540eb05f95713ec22d4859dc19fb
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-03-09 10:55:36 +0100

    introduction to javscript with mr oche

---
+++ app.js
@@ -0,0 +1,91 @@
+// what is a programming language 
+// a programming language is a set of 
+// instructions giving to a computer to perform a specific task 
+
+// what is a variable in js 
+// a variable is a container 
+
+// to declare a variable in javascript we can use either 
+// let, var or const
+
+let Name = 'emmanuel'; //a string datatype 
+console.log(Name)
+
+// i have to install node js on my system
+// i also installed code runner extension from vscode 
+
+var Student = 'Oche'
+
+var Student = 'Steven'
+console.log(Student);
+
+Name = 'Ogechi'
+console.log(Name);
+
+// let talk about const 
+// const is a variable declarator is fixed and cannot change 
+
+const pi = 3.14 
+console.log(pi);
+
+
+// lets talk about dataTypes in javascript
+
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+let number = 200
+console.log(number + number)
+
+let Raining = true
+let notRaining = false
+
+console.log(Raining);
+
+// Array in js 
+// An array is a list of elements seprated by comma 
+
+fruits = ['apple', 'banana', ',orange']
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits.length);
+
+// Object in javascript 
+// a specific dataTypes in javascript that has keys and values 
+
+Person = {Name: 'John', lastName: 'Doe', age:50, birthDate: 2024}
+
+Product_info = {Brand: 'Iphone', Model: 'Iphone 15 pro max', Date:'24/3/24', buying:true}
+
+console.log(Product_info.Brand);
+
+
+
+const products = [
+    { id: 1, name: 'Product 1', price: 20.00 },
+    { id: 2, name: 'Product 2', price: 15.00 },
+    { id: 3, name: 'Product 3', price: 30.00 }
+]
+
+
+const cart = JSON.parse(localStorage.getItem('product-list'))
+
+
+
+
+
+
